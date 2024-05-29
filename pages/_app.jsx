6fcb8cf91f11ed6d66f92c96d99e455fcceaf918
hari <hariaum2003@gmail.com>
2024-05-29 12:02:29 +0530

    Made the students page responsive and added horizontal scrollbars

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,11 @@
+// app.jsx
+import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 
 import Layout from "../components/Layout";
 import Transition from "../components/Transition";
+import theme from "../components/theme.js"; 
 
 import "../styles/globals.css";
 
@@ -10,14 +13,16 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full">
-          <Transition />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <AnimatePresence mode="wait">
+          <motion.div key={router.route} className="h-full">
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
   );
 }
 
